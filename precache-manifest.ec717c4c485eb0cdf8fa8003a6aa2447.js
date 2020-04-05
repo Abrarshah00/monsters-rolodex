c864c1eeadd254e446c667c4061c9094e66329de
Abrarshah00 <abrarshah00@gmail.com>
2020-04-05 20:21:31 +0530

    Updates

---
+++ precache-manifest.ec717c4c485eb0cdf8fa8003a6aa2447.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58785b110d70719863a26d7208024b2b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "fb80a84c3c5c715077d4",
+    "url": "/monster-rolodex/static/css/main.3f12a950.chunk.css"
+  },
+  {
+    "revision": "8a6eda421bc5765c55c2",
+    "url": "/monster-rolodex/static/js/2.bc65f737.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.bc65f737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb80a84c3c5c715077d4",
+    "url": "/monster-rolodex/static/js/main.c21eb540.chunk.js"
+  },
+  {
+    "revision": "bc09d86fba44e1c7ba83",
+    "url": "/monster-rolodex/static/js/runtime-main.8000f3b5.js"
+  }
+]);
